b01e5583965878c923ca0b4ea1f9e50f2afbc1c3
jyoti140220 <71928435+jyoti140220@users.noreply.github.com>
2020-12-14 09:28:44 -0800

    Add files via upload

---
+++ remove_1st_keys.py
@@ -0,0 +1,14 @@
+dic= {
+        1: 'NAVGURUKUL',
+        2: 'IN',  
+  	    3:{    
+             'A' : 'WELCOME',
+             'B' : 'To',
+             'C' : 'DHARAMSALA'
+            }
+        }
+# dic[3].pop('A')
+# print(dic)
+
+del dic[3]['A']
+print(dic)
